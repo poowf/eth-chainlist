84e9919d264e0987fb27f7efe105852e9b5fe00b
zanechua <zane@poowf.com>
2023-05-28 00:39:27 +0000

    chore: update chain data

--- src/data/chain.js
+++ src/data/chain.js
@@ -8311,6 +8311,24 @@ const chainArray = [
     ]
   },
   {
+    name: 'Canxium Mainnet',
+    chain: 'CAU',
+    rpc: [ 'https://rpc.canxium.org' ],
+    faucets: [],
+    nativeCurrency: { name: 'Canxium', symbol: 'CAU', decimals: 18 },
+    infoURL: 'https://canxium.org',
+    shortName: 'cau',
+    chainId: 3003,
+    networkId: 3003,
+    explorers: [
+      {
+        name: 'canxium explorer',
+        url: 'https://explorer.canxium.org',
+        standard: 'none'
+      }
+    ]
+  },
+  {
     name: 'Orlando Chain',
     chain: 'ORL',
     rpc: [ 'https://rpc-testnet.orlchain.com' ],
