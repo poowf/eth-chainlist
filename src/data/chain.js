aaf215907b6b15fce17f88049237155098feba7c
zanechua <zane@poowf.com>
2024-02-27 00:32:52 +0000

    chore: update chain data

--- src/data/chain.js
+++ src/data/chain.js
@@ -17519,6 +17519,25 @@ const chainArray = [
     parent: { type: 'L2', chain: 'eip155-248' }
   },
   {
+    name: 'Decentraconnect Social',
+    chain: 'DCSM',
+    icon: 'dcsmIcon',
+    rpc: [ 'https://rpc.decentraconnect.io' ],
+    faucets: [],
+    nativeCurrency: { name: 'Decentraconnect Social', symbol: 'DCSM', decimals: 18 },
+    infoURL: 'https://docs.decentraconnect.io',
+    shortName: 'DCSMs',
+    chainId: 19224,
+    networkId: 19224,
+    explorers: [
+      {
+        name: 'Decentraconnect Social',
+        url: 'https://decentraconnect.io',
+        standard: 'EIP3091'
+      }
+    ]
+  },
+  {
     name: 'Magnet Network',
     chain: 'Magnet',
     rpc: [ 'https://magnet-rpc.magport.io/' ],
