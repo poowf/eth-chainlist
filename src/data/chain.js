1c38faba96afa2ae3a945e088a2370ec6870ad7c
zanechua <zane@poowf.com>
2025-12-14 01:01:04 +0000

    chore: update chain data

--- src/data/chain.js
+++ src/data/chain.js
@@ -39612,34 +39612,18 @@ const chainArray = [
     explorers: []
   },
   {
-    name: 'Taiko Hekla',
+    name: 'Taiko Hekla (deprecated)',
     chain: 'ETH',
-    status: 'active',
+    status: 'deprecated',
     icon: 'taiko',
-    rpc: [
-      'https://rpc.hekla.taiko.xyz',
-      'wss://ws.hekla.taiko.xyz',
-      'https://taiko-hekla-rpc.publicnode.com',
-      'wss://taiko-hekla-rpc.publicnode.com'
-    ],
+    rpc: [],
     faucets: [],
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     infoURL: 'https://taiko.xyz',
     shortName: 'tko-hekla',
     chainId: 167009,
     networkId: 167009,
-    explorers: [
-      {
-        name: 'blockscout',
-        url: 'https://blockscoutapi.hekla.taiko.xyz',
-        standard: 'EIP3091'
-      },
-      {
-        name: 'Routescan',
-        url: 'https://hekla.taikoexplorer.com',
-        standard: 'EIP3091'
-      }
-    ]
+    explorers: []
   },
   {
     name: 'Taiko Hoodi',
@@ -39655,7 +39639,12 @@ const chainArray = [
     networkId: 167013,
     explorers: [
       {
-        name: 'taikoscan',
+        name: 'Blockscout',
+        url: 'https://blockscout.hoodi.taiko.xyz',
+        standard: 'EIP3091'
+      },
+      {
+        name: 'Etherscan',
         url: 'https://hoodi.taikoscan.io',
         standard: 'EIP3091'
       }
